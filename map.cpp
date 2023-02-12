2b3f69036f54075853237f9733ec22df883a98b7
AmritSohpal <amritsohpal@gmail.com>
2023-02-12 18:31:03 -0500

    uploaded map.h map.cpp
    below is the code for territory,map,continent and mapLoader, mapDriver will be done shortly.

---
+++ map.cpp
@@ -0,0 +1,220 @@
+#include "map.h"
+
+using namespace std;
+//CLASS PLAYER 
+Player::Player(const string& x){
+    name = x;
+}
+Player::~Player() {}
+string Player::getName(){return name;}
+
+
+
+//CLASS TERRITORY ************************************************************************************************************************
+//Constructor Territory 
+Territory::Territory(int idTerritory, string usrName, string usrContinent, shared_ptr<Player> currentPlayer, int armyCount) {
+    territoryId = unique_ptr<int>(new int(idTerritory));
+    name = unique_ptr<string>(new string(usrName));
+    belongsToContinent = unique_ptr<string>(new string(usrContinent));
+    playerInPossession = shared_ptr<Player>(currentPlayer);
+    armies = unique_ptr<int>(new int(armyCount));
+}
+Territory::Territory(int idTerritory,string usrName, string usrContinent) {
+    territoryId = unique_ptr<int>(new int(idTerritory));
+    name = unique_ptr<string>(new string(usrName));
+    belongsToContinent = unique_ptr<string>(new string(usrContinent));
+    playerInPossession = nullptr;
+    armies = unique_ptr<int>(new int(0));
+}
+ostream& operator<<(std::ostream& os, const Territory& territoryInfo) {
+    os << "Name: " << *territoryInfo.name << 
+    ", Continent: " << *territoryInfo.belongsToContinent 
+    << ", Player: " << territoryInfo.playerInPossession->getName() 
+    << ", Armies: " << *territoryInfo.armies << endl;
+    return os;
+}
+
+Territory::~Territory() {
+/*  Since we have smart pointers, "delete" pointers would not be required nor would it work in our scenario,
+    as smart pointers delete themselves.*/
+
+/*  Although smart pointers deallocate memories themselves, we can use .reset() to ensure that our pointers are 
+    destroyed. Since we have unique_ptr's we can be certain that when destroying these pointers, we wont have 
+    any dangling pointers; 
+*/
+    name.reset();
+    belongsToContinent.reset();
+    playerInPossession.reset();
+    armies.reset();
+}
+
+Territory& Territory::operator=(const Territory &copiedTerritory) {
+    /* Smart pointers automatically manage for memory leaks therefor we would not have to
+       delete pointers*/
+    name.reset();
+    belongsToContinent.reset();
+    playerInPossession.reset();
+    armies.reset();
+    // Allocate new memory and copy over the data from the other object
+    name = unique_ptr<string>(new string(*copiedTerritory.name));
+    belongsToContinent = unique_ptr<string>(new string(*copiedTerritory.belongsToContinent));
+    armies = unique_ptr<int>(new int(*copiedTerritory.armies));
+    playerInPossession = shared_ptr<Player>(new Player(*copiedTerritory.playerInPossession));
+   // This is a pointer that deals with our current object, by returning this, we are returning the 
+   //current object 
+    return *this;
+}
+void Territory::setName(const string &newName) {
+    *name = newName;
+}
+void Territory::setContinent(const string &newContinent) {
+    *belongsToContinent = newContinent;
+}
+void Territory::setArmy(int armyCount){
+    *armies = armyCount;
+}
+void Territory::setNeighbours(Territory* N){
+    adjacent.push_back(N);
+}
+//mutator functions 
+string Territory::getTerritoryName() const{
+    return *name;
+}
+string Territory::getContinent() const {
+    return *belongsToContinent;
+}
+int Territory::getArmyCount() const {
+    return *armies;
+}
+
+
+
+
+//CLASS CONTINENTS ********************************************************************************************************************************
+Continent::Continent(string continentName, int idContinent){
+    name = unique_ptr<string>(new string(continentName));
+    continentID = unique_ptr<int>(new int(idContinent));
+}
+
+Continent::~Continent() {}
+
+void Continent::addTerritory(Territory* A ){
+    territories.push_back(A);
+}
+
+string Continent::getContinentName(){
+    return *name;
+}
+int Continent::getContinentID(){
+    return *continentID;
+}
+
+
+
+
+
+//CLASS MAP ***************************************************************************************************************************
+//constructor and deconstructor
+Map::Map(){}
+Map::~Map(){}
+
+//mutator functions
+void Map::addTerritory(Territory* A){
+    territories.push_back(A);
+    territoryMap[A->getTerritoryName()] = A;
+
+}
+void Map::addContinent(Continent* A){
+    continents.push_back(A);
+    continentMap[A->getContinentName()] = A;
+}
+void Map::connectTerritories(string A,string B){
+     Territory* territory1 = territoryMap[A];
+     Territory* territory2 = territoryMap[B];
+     territory1->setNeighbours(territory2);
+     territory2->setNeighbours(territory1);
+}
+
+//accessor functions 
+string Map::getContinentFromContinentId(int id){
+    for(Continent* c: continents){
+        if(c->getContinentID() == id){
+            return c->getContinentName();  
+        }
+        else {
+            return "NOT A CONTINENT";
+        }
+    }
+    return "";
+}
+
+bool Map::isConnectedMap() {
+    vector<Territory*> currentTerritories = territories;
+    while (currentTerritories.size() > 0) {
+        auto neighbour = currentTerritories[0];
+        int index = std::distance(territories.begin(), find(territories.begin(), territories.end(), neighbour));
+
+        for (int i = 0; i < neighbour->getAdjacent().size(); i++) {
+            auto& nextTerritory = neighbour->getAdjacent()[i];
+            if (find(currentTerritories.begin(), currentTerritories.end(), nextTerritory) != currentTerritories.end()) {
+                int index = std::distance(territories.begin(), find(territories.begin(), territories.end(), nextTerritory));
+            }
+        }
+    }
+
+    return currentTerritories.size() == 0;
+}
+
+
+//******************************************************
+//README*************************************************
+//NEED TO IMPLEMENT INONECONTINENT**************************
+//***********************************************************
+
+
+//MAPLOADER CLASS ******************************************************************************
+MapLoader::MapLoader(const string& fileName){
+        ifstream mapFile(fileName);
+        int continentCounter =0;
+
+        if (!mapFile.is_open()) {
+            cout << "Error: Unable to open file" << endl;
+            throw std::runtime_error("Error: Unable to open file");
+        }
+
+        string line;
+        while (getline(mapFile, line)) {
+            if (line == "[continents]") {
+                while (getline(mapFile, line) && !line.empty()) {
+                    continentCounter++;
+                    istringstream getWord(line);
+                    string continentName;
+                    getWord >> continentName;
+                    Continent *newContinent = new Continent(continentName, continentCounter);
+                    mp ->addContinent(newContinent);
+                }
+            }
+
+            if (line == "[countries]") {
+                while (getline(mapFile, line) && !line.empty()) {
+                    istringstream getWord(line);
+                    int countryID, continentName;
+                    string countryName;
+                    getWord >> countryID >> countryName >> continentName;
+                        
+                }
+            }
+
+            if (line == "[borders]") {
+                while (getline(mapFile, line) && !line.empty()) {
+                    istringstream getWord(line);
+                    string getBorders, continentId;
+                    std::stringstream getInt(line);
+                    int node,first;
+                    getInt >> first;
+                    while (getInt >> node) {
+                    }
+                }
+            }
+        }
+}
---
+++ map.h
@@ -0,0 +1,128 @@
+#ifndef MAP_H 
+#define MAP_H
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <map>
+#include <algorithm>
+#include <stack>
+
+//TESTER CLASS OFF GITHUB, NOT PART OF THE ASSIGNMENT CRITERIA *********************
+using namespace std;
+class Player {
+private:
+    string name;                         //player name
+
+public:
+    Player();                            //default constructor
+    Player(const string&);              //one parameter constructor
+    Player(const Player*);               //copy constructor DELETE***********************
+    Player& operator = (const Player&);  //assignment operator DELETE ***************************************
+    ~Player();                           //destructor
+
+    string getName();                    //accessor
+    void setName(string);                //mutator
+};
+//TESTER CLASS END ********************************************
+
+
+//Class TERRITORY ***********************************************************************************
+//Territory Class 
+class Territory{
+    private:
+    unique_ptr<std::string> name;
+    unique_ptr<std::string> belongsToContinent;
+    shared_ptr<Player> playerInPossession;
+    unique_ptr<int> armies;
+    unique_ptr<int> territoryId;
+    vector<Territory*> adjacent;
+
+    public:
+    //Constructors and Destructors
+    Territory( int, string,  string , shared_ptr<Player>,int);
+    Territory(int, string ,string);
+    Territory(const Territory& );
+     ~Territory();
+     Territory& operator=(const Territory&);
+    
+     /* Stream insertion "<<" is not a member of our class, it belongs to <iostream>.
+        Therefor to work with Stream insertion our operator overloading */
+    friend std::ostream& operator<<(std::ostream&, const Territory&); 
+    
+    
+    //Accessor functions 
+    void setName(const string&);
+    void setContinent(const string&) ; 
+    void setArmy(int);
+    void setNeighbours(Territory* );
+    
+    //mutator functions 
+    string getTerritoryName() const;
+    string getContinent() const; 
+    int getArmyCount() const;
+    vector<Territory*> getAdjacent() const { return adjacent; }
+
+    //NEED TO UPDATE WITH EUGENIE'S CLASS 
+    // Player getPlayer() const; 
+    //void setPlayer(Player*);
+};
+
+
+//Class CONTINENT ******************************************************************************
+class Continent {
+private:
+    unique_ptr<string>name;
+    unique_ptr<int>continentID;
+    vector<Territory*> territories;
+public:
+    Continent(string,int);
+    ~Continent();
+
+    void addTerritory(Territory* );
+
+    string getContinentName();
+    int getContinentID();
+    //FOR BOOL isConnectedContinent ONLYYY
+    vector<Territory*> getTerritories() const { return territories; }
+};
+
+//class MAP ***********************************************************************
+class Map{
+    public:
+    vector<Territory*> territories;
+    vector<Continent*> continents;
+    map<string, Territory*> territoryMap;
+    map<string, Continent*> continentMap;
+
+    Map(); //default constructor
+    ~Map();
+
+    //mutator functions
+    void addTerritory(Territory* );
+    void addContinent(Continent* );
+    void connectTerritories(string,string);
+
+    //boolean functions
+    bool isConnectedMap();
+    bool isConnectedContinents();
+    bool inOneContinent(string);
+
+    //accessor function
+    string getContinentFromContinentId(int);
+
+
+};
+
+class MapLoader{
+    private:
+    shared_ptr<Map> mp;
+    public:
+    MapLoader(const string&);
+};
+
+#endif
