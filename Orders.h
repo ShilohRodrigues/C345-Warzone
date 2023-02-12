94d4a42701f5cc594e35bd6522b6083fa3ca0f4b
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-02-12 17:15:47 -0500

    Skeleton for OrdersDriver.cpp

--- Orders.h
+++ Orders.h
@@ -108,7 +108,6 @@ public:
     void move(const string& direction, int orderID);
     void remove(int orderID);
 
-private:
     unique_ptr<list<shared_ptr<Order>>> orderList;
 };
 
--- OrdersDriver.cpp
+++ OrdersDriver.cpp
@@ -3,5 +3,32 @@
 using namespace std;
 
 void ordersDriver() {
-    cout << "ordersDriver";
+    // create order of every kind
+    auto testDeploy = make_shared<Deploy>();
+    auto testAdvance = make_shared<Advance>();
+    auto testBomb = make_shared<Bomb>();
+    auto testBlockade = make_shared<Blockade>();
+    auto testAirlift = make_shared<Airlift>();
+    auto testNegotiate = make_shared<Negotiate>();
+
+    // create a list of orders
+    auto testOrdersList = make_unique<OrdersList>();
+    testOrdersList->add(testDeploy);
+
+    cout << "== OrdersList Stream Insertion Operator ==\n" << testOrdersList << endl;
+
+    for (const auto& order : *testOrdersList->orderList) {
+        // every order subclass has validate()
+        order->validate();
+        // every order subclass has execute()
+        order->execute();
+    }
+
+    // OrdersList has remove()
+    testOrdersList->remove(3); // remove blockade
+    cout << "== OrdersLost::remove() ==\n" << testOrdersList << endl;
+
+    //OrdersList has move()
+    testOrdersList->move("up", 2); // remove blockade
+    cout << "== OrdersLost::move() ==\n" << testOrdersList << endl;
 }
