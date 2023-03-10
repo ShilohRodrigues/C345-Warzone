5853e6f9b4d79c7403cbaec46cf3d73e4edb4cb1
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-03-10 17:06:36 -0500

    Added a main driver for demos

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "Cards.h"
+#include "GameEngine.h"
+#include "Orders.h"
+#include "Player.h"
+#include "Map.h"
+
+int main() {
+//    cardDemo1();
+//    gameEngineDemo1();
+//    ordersDemo1();
+//    playerDemo1();
+    mapDemo1();
+
+}
