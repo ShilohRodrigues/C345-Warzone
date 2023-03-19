7d875339473c858d1f219fa1477c51ff24251ce3
Shiloh Rodrigues <shilohrodrigues@hotmail.com>
2023-03-19 00:47:31 -0400

    Revert "added 3 phases, sort order "
    This reverts commit fb18043291cc880741b2c86ecc08ff8aa3384685.

--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -3,22 +3,6 @@
 
 using namespace std;
 
-
-void mainGameLoop(GameEngine* game) {
-
-    bool gameEnded = false;
-
-    while (!gameEnded) {
-        game->reinforcementPhase();
-        game->issueOrdersPhase();
-
-        if (game->executeOrdersPhase() == 0) {
-            gameEnded = true;
-            cout << "Game ended, Thank you for playing!" << endl;
-        }
-    }
-}
-
 void gameEngineDemo1() {
 
   //Create game object
@@ -26,27 +10,25 @@ void gameEngineDemo1() {
 
   cout << "Welcome to Warzone" << endl;
 
-  mainGameLoop(game);
+  //Main game loop
+  while(true) {
 
-  ////Main game loop
-  //while(true) {
+    cout << *game << endl << endl; //Print current game state
+    cout << "Enter a command to continue: ";
 
-  //  cout << *game << endl << endl; //Print current game state
-  //  cout << "Enter a command to continue: ";
+    //Get entered command, check return status, 1 for error, 2 for game ended
+    string input = ""; 
+    getline(cin, input);
 
-  //  //Get entered command, check return status, 1 for error, 2 for game ended
-  //  string input = ""; 
-  //  getline(cin, input);
+    if (input == "quit") break;
+    int status = game->nextState(input);
+    if (status == 1) cout << "Invalid command entered: " << input << endl << endl;
+    if (status == 2) {
+      cout << "Game ended, Thank you for playing!" << endl;
+      break;
+    } 
 
-  //  if (input == "quit") break;
-  //  int status = game->nextState(input);
-  //  if (status == 1) cout << "Invalid command entered: " << input << endl << endl;
-  //  if (status == 2) {
-  //    cout << "Game ended, Thank you for playing!" << endl;
-  //    break;
-  //  } 
-
-  //}
+  }
   
   delete game;
 
